685fec8a8dda0206cc2fc63a6ccd9d7a6eda43fa
Stuart Baly <stuartbaly@gmail.com>
2018-03-20 23:12:08 +1100

    First compiling and flashing version. Press button for fast flashing.

--- minimum.c
+++ minimum.c
@@ -11,6 +11,29 @@ void SystemInit()
 
 int main(void)
 {
-	rcc_periph_clock_enable(RCC_GPIOA);
-	for (;;);
+  int temp;
+  int rate;
+  
+	rcc_periph_clock_enable(RCC_GPIOB);
+	
+	/* PB1 (LED) output */
+	gpio_set_mode(GPIOB,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO1);
+	
+	/* PB8 (button) input */
+	gpio_set_mode(GPIOB,GPIO_MODE_INPUT,GPIO_CNF_INPUT_FLOAT,GPIO8);
+	
+	/* start with LED off */
+	gpio_clear(GPIOB,GPIO1);
+	
+	/* loop forever */
+	while (1) {
+	  if (gpio_get(GPIOB,GPIO8)) {
+	    rate=30000;
+	  }
+	  else {
+	    rate=100000;
+	  }
+	  for (temp=0;temp<rate;temp++);
+	  gpio_toggle(GPIOB,GPIO1);
+	}
 }
